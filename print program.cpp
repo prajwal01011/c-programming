804a83a77856268a2af583ab8d0bee57c560c6c6
Prajwal Baskota <prajwalbaskota11@gmail.com>
2026-03-30 22:47:46 +0545

    first commit

--- print program.cpp
+++ print program.cpp
@@ -2,5 +2,27 @@
 int main ()
 {
 	printf("sayal muji");
+	int a,b,sum;
+	for(a=0;a<=3;a++)
+	{
+		for(sum=0;sum<=a/2;sum++){
+		
+		printf(" ");
+	}
+		 
+		for(b=0;b<=3;b++)
+		{
+			printf (" b ");
+		}
+		for(sum=0;sum<=a/2;sum++)
+		{
+			printf(" ");
+		}
+	
+			if(a==b) 
+		{
+		printf("\n");
+		break;
+		}
 	return 0;
 }
