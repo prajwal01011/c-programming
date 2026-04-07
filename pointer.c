366c35b0d1847009d47e7271dc28cd42d2c50f8c
Prajwal Baskota <prajwalbaskota11@gmail.com>
2026-04-07 21:45:51 +0545

    pointer learn

--- pointer.c
+++ pointer.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main(){
     int age=22;
-    int *ptr=age;
+    int *ptr= &age;
     printf("%u\n",&age);
     printf("%u\n", ptr);
+    printf("%U\n", &ptr);
     return 0;
 }
